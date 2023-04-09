661a276870512657b08d01bc9ef9b490e8c0d797
Sahil Ambure <76648609+sahilambure94@users.noreply.github.com>
2023-04-09 18:54:30 +0530

    loading screen provider

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:instagramclone_flutter/state/auth/models/auth_result.dart';
 import 'package:instagramclone_flutter/state/auth/providers/auth_state_provider.dart';
+import 'package:instagramclone_flutter/state/providers/is_loading_provider.dart';
+import 'package:instagramclone_flutter/views/components/loading/loading_screen.dart';
 import 'firebase_options.dart';
-import 'dart:developer' as devtools show log;
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-
-extension Log on Object {
-  void log() => devtools.log(toString());
-}
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -24,19 +21,35 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData.dark(),
       darkTheme: ThemeData.dark().copyWith(indicatorColor: Colors.blueGrey),
       debugShowCheckedModeBanner: false,
-      home: Consumer(builder: ((context, ref, child) {
-        final isLoggedIn =
-            ref.watch(authStateProvider).result == AuthResult.success;
-        if (isLoggedIn) {
-          return const MainView();
-        } else {
-          return const LoginView();
-        }
-      })),
+      home: Consumer(
+        builder: (context, ref, child) {
+          // display loading screen
+
+          ref.listen<bool>(
+            isLoadingProvider,
+            (_, isLoading) {
+              if (isLoading) {
+                LoadingScreen.instance().show(
+                  context: context,
+                );
+              } else {
+                LoadingScreen.instance().hide();
+              }
+            },
+          );
+
+          final isLoggedIn =
+              ref.watch(authStateProvider).result == AuthResult.success;
+          if (isLoggedIn) {
+            return const MainView();
+          } else {
+            return const LoginView();
+          }
+        },
+      ),
     );
   }
 }
---
+++ lib/state/providers/is_loading_provider.dart
@@ -0,0 +1,7 @@
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:instagramclone_flutter/state/auth/providers/auth_state_provider.dart';
+
+final isLoadingProvider = Provider((ref) {
+  final authState = ref.watch(authStateProvider);
+  return authState.isLoading;
+});
