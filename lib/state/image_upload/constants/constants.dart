87dbe52cb29f96ea3bdf3373082f5eb956464559
Sahil Ambure <76648609+sahilambure94@users.noreply.github.com>
2023-04-10 17:31:40 +0530

    posts settings, notifier and provider added

---
+++ lib/state/image_upload/constants/constants.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/foundation.dart' show immutable;
+
+@immutable
+class Constants {
+  // photos
+  static const imageThumbnailWidth = 150;
+  // videos
+  static const videoThumbnailMaxHeight = 400;
+  static const videoThumbnailQuality = 75;
+  const Constants._();
+}
--- lib/state/posts/models/post_payload.dart
+++ lib/state/posts/models/post_payload.dart
@@ -25,5 +25,14 @@ class PostPayload extends MapView<String, dynamic> {
           PostKey.createdAt: FieldValue.serverTimestamp(),
           PostKey.thumbnailUrl: thumbnailUrl,
           PostKey.fileUrl: fileUrl,
+          PostKey.fileType: fileType.name,
+          PostKey.fileName: fileName,
+          PostKey.aspectRatio: aspectRatio,
+          PostKey.thumbnailStorageId: thumbnailStorageId,
+          PostKey.originalFileStorageId: originalFileStorageId,
+          PostKey.postSettings: {
+            for (final postSetting in postSettings.entries)
+              postSetting.key.storageKey: postSetting.value,
+          },
         });
 }
